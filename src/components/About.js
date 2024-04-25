b2ab9733afc70d0e6d5397e9129ca83cef206df5
benard <benardboiwo703@gmail.com>
2024-04-25 11:08:34 +0300

    commit initial

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+
+
+function Bio(props) {
+  const bioPresent = props.Bio
+  if (bioPresent) {
+    return <p>{props.Bio}</p>
+  }
+  else{
+    return null
+  }
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <Bio Bio={props.bio}/>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links/>
     </div>
   );
 }
+
 
 export default About;
